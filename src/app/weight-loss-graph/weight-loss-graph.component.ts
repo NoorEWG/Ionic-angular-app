0ddc4e8e8a32afbab258f1d7a1f45e07410cc8d9
Erica de Graaf-Borkent <erica.de.graaf.borkent@gmail.com>
2019-06-21 07:36:19 +0200

    Change graphs, remove empty values

--- src/app/weight-loss-graph/weight-loss-graph.component.ts
+++ src/app/weight-loss-graph/weight-loss-graph.component.ts
@@ -21,9 +21,11 @@ export class WeightLossGraphComponent implements OnInit {
   @Input('categories') categories: string[];
   @Input('series') series: number[];
   @Input('series2') series2: number[];
+  @Input('series3') series3: number[];
   @Input('chartType') chartType: string;
   @Input('title') title: string;
   @Input('title2') title2: string;
+  @Input('title3') title3: string;
   @Input('opposite') opposite: boolean;
   @Input('startWeight') startWeight: number;
   @Input('targetWeight') targetWeight: number;
@@ -96,7 +98,7 @@ export class WeightLossGraphComponent implements OnInit {
     ];
     
     if(this.measure) {
-      mesure = ' in ' + this.measure;
+      mesure = ' (' + this.measure + ')';
     }
     else {
       mesure = ' ';
@@ -128,15 +130,6 @@ export class WeightLossGraphComponent implements OnInit {
           zones: this.zones
         },
       },
-      xAxis: {
-        categories: this.categories,
-        type: 'datetime',
-        labels: {
-          formatter: function() {
-            return moment(this.value).format("DD/MM/YYYY");
-          }
-        }
-      },
       yAxis: [{
         title: {
           text: this.title + mesure
@@ -167,7 +160,27 @@ export class WeightLossGraphComponent implements OnInit {
     };
   
     this.series = this.correctSeries(this.series);
-  
+    if(this.series2 && this.series2.length > 0) {
+      this.series2 = this.correctSeries(this.series2);
+    }
+    if(this.series3 && this.series3.length > 0) {
+      this.series3 = this.correctSeries(this.series3);
+    }
+    
+   
+    
+    this.removeEmpty();
+
+    options.xAxis = {
+      categories: this.categories,
+      type: 'datetime',
+      labels: {
+        formatter: function() {
+          return moment(this.value).format("DD/MM/YYYY");
+        }
+      }
+    } 
+
     if(this.chartType === 'column') {
       options.chart.type = this.chartType;
       options.chart.options3d = {
@@ -187,12 +200,18 @@ export class WeightLossGraphComponent implements OnInit {
     }
     else if(this.chartType === 'spline') {
       options.series.push( <Highcharts.SeriesSplineOptions> {type: this.chartType, name: this.title, data: this.series, yAxis: 0});
+      options.xAxis = {
+        categories: this.categories,
+        labels: {
+          enabled : true
+        }
+      }
     }
     else if(this.chartType === 'line') {
       options.series.push( <Highcharts.SeriesLineOptions> {type: this.chartType, name: this.title, data: this.series, yAxis: 0});
     }
     else {
-      options.series.push( <Highcharts.SeriesAreasplineOptions> {type: this.chartType, name: this.title, data: this.series, yAxis: 0});
+      options.series.push( <Highcharts.SeriesAreasplineOptions> {type: this.chartType, name: this.title, data: this.series, yAxis: 0}); 
     }
     // min and max graph  
     options.yAxis[0].max = Math.max(...this.series) + 1;
@@ -201,7 +220,6 @@ export class WeightLossGraphComponent implements OnInit {
 
     // graph with 2 axis
     if(this.series2) {
-      this.series2 = this.correctSeries(this.series2);
       var noZeros2 = this.calculateArrayWithoutZeros(this.series2); 
       var yAxisNumber = 0;
       if(this.opposite) {
@@ -235,7 +253,43 @@ export class WeightLossGraphComponent implements OnInit {
     else {
       options.yAxis[1].visible = false;
     }
-  
+
+     // graph with 3 series
+     if(this.series3) {
+      var noZeros3 = this.calculateArrayWithoutZeros(this.series3); 
+      var yAxisNumber = 0;
+      if(this.opposite) {
+        yAxisNumber = 1;
+        options.yAxis[1].opposite = true;
+        // min and max graph  
+        options.yAxis[1].max = Math.max(...this.series3) + 1;  
+        options.yAxis[1].min = (Math.min(...noZeros3) - 2) < 0 ? 0 : Math.min(...noZeros3) - 1;
+        options.yAxis[1].visible = true;
+      }
+      else {
+        if(this.title2 && this.title3) {
+          options.yAxis[0].title.text = this.title + " / " + this.title2 + " / " + this.title3 + mesure;
+          options.title.text =  this.title + " / " + this.title2 + " / " + this.title3;
+        }
+        // recalculate min and max graph 
+        options.yAxis[0].max = Math.max(...this.series, ...this.series2, ...this.series3) + 1;
+        options.yAxis[0].min = (Math.min(...noZeros, ...noZeros2, ...noZeros3) - 2) < 0 ? 0 : Math.min(...noZeros, ...noZeros2, ...noZeros3) - 1;
+        options.yAxis[1].visible = false;
+      }
+      if(this.chartType === 'column') {
+        options.series.push( <Highcharts.SeriesColumnOptions> {type: this.chartType, stack: "1", name: this.title3, data: this.series3, yAxis: yAxisNumber});
+      }
+      else if(this.chartType === 'spline') {
+        options.series.push( <Highcharts.SeriesSplineOptions> {type: this.chartType, name: this.title3, data: this.series3, yAxis: yAxisNumber});
+      }
+      else {
+        options.series.push( <Highcharts.SeriesAreasplineOptions> {type: this.chartType, name: this.title3, data: this.series3, yAxis: yAxisNumber});
+      }
+    }
+    else {
+      options.yAxis[1].visible = false;
+    }
+     
     this.chart = new Highcharts.Chart(this.chartTarget.nativeElement, options);  
   }
 
@@ -257,5 +311,45 @@ export class WeightLossGraphComponent implements OnInit {
       }
     }
     return noZeros;
+  }
+
+  removeEmpty() {
+    // correction for not registered values / weeks
+    var series = new Array<number>();
+    var series2 =  new Array<number>();
+    var series3 =  new Array<number>();
+    var categories =  new Array<string>();
+    for(var i = 0; i < this.series.length; i++) {
+      if(this.series3 && this.series3.length > 0) {
+        if(this.series3[i] > 0 || this.series2[i] > 0 || this.series[i] > 0) {
+          series.push(this.series[i]);
+          series2.push(this.series2[i]);
+          series3.push(this.series3[i]);
+          // categories.push(moment(this.categories[i]).format('DD-MM-YYYY'));
+          categories.push(this.categories[i]);
+        }
+      }
+      else if(this.series2 && this.series2.length > 0) {
+        if (this.series2[i] > 0 || this.series[i] > 0) {
+          series.push(this.series[i]);
+          series2.push(this.series2[i]);
+          categories.push(this.categories[i]);
+        }
+      }
+      else {
+        if(this.series[i] > 0) {
+          series.push(this.series[i]);
+          categories.push(this.categories[i]);
+        }
+      }
+    }
+    this.series = series;
+    if(this.series2 && this.series2.length > 0) {
+      this.series2 = series2;
+    }
+    if(this.series3 && this.series3.length > 0) {
+      this.series3 = series3;
+    }
+    this.categories = categories; 
   }
 }
